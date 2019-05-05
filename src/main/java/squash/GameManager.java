bcc58d39db8c7176d719166c3021b92ba1c6f931
Kaito Sato <ksato.nit@gmail.com>
2019-05-05 14:38:59 +0900

    [add] ゲームセットアップの処理

--- src/main/java/squash/GameManager.java
+++ src/main/java/squash/GameManager.java
@@ -1,16 +1,29 @@
 package squash;
 
 public class GameManager {
+    private enum PHASE {
+        TITLE,
+        PLAY,
+        RESULT,
+    }
+
     private static GameManager gameManagerInstance = new GameManager();
 
     public static final int WIDTH = 640;
     public static final int HEIGHT = 960;
 
+    public PHASE gamePhase;
+
     private GameManager(){
         // Initialize
+        this.setUp();
     }
 
     public static GameManager getInstance(){
         return gameManagerInstance;
+    }
+
+    private void setUp(){
+        this.gamePhase = PHASE.TITLE;
     }
 }
---
+++ src/main/java/squash/Player.java
@@ -0,0 +1,4 @@
+package squash;
+
+public class Player {
+}
